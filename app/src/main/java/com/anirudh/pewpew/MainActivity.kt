be4c82d48821bacf117ce3c23a1475f559fe5029
anirudh797 <anirudh.kaptiyal77@gmail.com>
2023-04-12 00:20:23 +0530

    Initial commit

---
+++ app/src/main/java/com/anirudh/pewpew/MainActivity.kt
@@ -0,0 +1,31 @@
+
+package com.anirudh.pewpew
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    menuInflater.inflate(R.menu.menu_main, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    return when (item.itemId) {
+      R.id.action_sequels -> {
+        startActivity(Intent(this, SequelsActivity::class.java))
+        true
+      }
+      else -> super.onOptionsItemSelected(item)
+    }
+  }
+}
